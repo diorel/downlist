0a73036d3d15f6c2f235587ef3cadee14d1435ef
diorel <diorel_x@hotmail.com>
2018-01-21 22:28:48 -0600

    boton lista
    boton lista mvc

---
+++ DropDownListMVC/DropDownListMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DropDownListMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownListMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            UsersData obj = new UsersData();
+            return View(obj);
+        }
+
+         [HttpPost]
+         [AllowAnonymous]
+         public async Task<ActionResult> Index(UsersData obj)
+         {
+            int d = obj.id;
+            Response.Write(d);
+            return View("Index",obj);
+
+        }
+
+
+
+    }
+}
---
+++ DropDownListMVC/DropDownListMVC/Models/UsersData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownListMVC.Models
+{
+    public class UsersData
+    {
+        [DataType(DataType.Text)]
+        [DisplayName("USers")]
+        public List<SelectListItem> usersLista { get; set; }
+
+        public int id { get; set; }
+
+        public UsersData()
+        {
+            PDHNEntities data = new PDHNEntities();
+            usersLista = new List<SelectListItem>();
+            var query = from u in data.USers select u;
+            var listData = query.ToList();
+
+            foreach (var Data in listData)
+            {
+                usersLista.Add(new SelectListItem()
+                {
+                    Value = Data.IdUser.ToString(),
+                    Text = Data.Name
+                });
+            }
+            
+        }
+    }
+}
